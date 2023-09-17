21579f041da6adcf2847dbdd7db48e5f001a0848
Chinh-Nguyenx <chinhndph25585@fpt.edu.vn>
2023-09-17 14:04:03 +0700

    laravel-17/9-book

--- book/routes/web.php
+++ book/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\layoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home',[layoutController::class,'index'])->name('layout-home');
